f5ac842be77fd8da9b53e21b12c8947bf8571486
song <songsm5504@gmail.com>
2020-07-08 20:57:11 +0900

    Updates

---
+++ precache-manifest.f38a49f7b90abeb84e8ef652df43ae28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c543f64867a31e75e8e42df65b47ad79",
+    "url": "/nomadcoders_movie_app/index.html"
+  },
+  {
+    "revision": "0269e85fdf0b3b581a85",
+    "url": "/nomadcoders_movie_app/static/css/main.692886c2.chunk.css"
+  },
+  {
+    "revision": "5ed518ac5c2b3acfc7a7",
+    "url": "/nomadcoders_movie_app/static/js/2.4baf104e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomadcoders_movie_app/static/js/2.4baf104e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0269e85fdf0b3b581a85",
+    "url": "/nomadcoders_movie_app/static/js/main.cd9d05cd.chunk.js"
+  },
+  {
+    "revision": "c78ef3f6c9668a1c9af3",
+    "url": "/nomadcoders_movie_app/static/js/runtime-main.32e423ff.js"
+  }
+]);
